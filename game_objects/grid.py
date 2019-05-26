dd63ddb8d3de538714ccc2909087f8e8450bccca
Erik Nordin <aeketn@gmail.com>
2019-05-25 21:22:05 -0700

    Modified field names of Grid's dimensions.
    - x_cells is now length.
    - y_cells is now height.

--- game_objects/grid.py
+++ game_objects/grid.py
@@ -11,8 +11,8 @@ __Y__ = 1
 # The Grid informs the GameWindow what to draw to the screen.
 class Grid:
     cell_size = None
-    x_cells = None
-    y_cells = None
+    length = None
+    height = None
     cells = None
     snake = None
 
@@ -21,21 +21,21 @@ class Grid:
         if 'cell_size' in kwargs:
             self.cell_size = kwargs.pop('cell_size', False)
         if 'length' in kwargs:
-            self.x_cells = kwargs.pop('length', False)
+            self.length = kwargs.pop('length', False)
         if 'height' in kwargs:
-            self.y_cells = kwargs.pop('height', False)
+            self.height = kwargs.pop('height', False)
 
-        row = [CellType.empty] * self.x_cells
+        row = [CellType.empty] * self.length
         self.get_cells = []
-        for _ in range(0, self.y_cells):
+        for _ in range(0, self.height):
             self.get_cells.append(row.copy())
 
         print(len(self.get_cells))
         print(len(self.get_cells[0]))
 
         start_direction = random.choice(list(Direction))
-        snake_x = random.randint(0, self.x_cells - 1)
-        snake_y = random.randint(0, self.y_cells - 1)
+        snake_x = random.randint(0, self.length - 1)
+        snake_y = random.randint(0, self.height - 1)
 
         self.snake = Snake(x=snake_x, y=snake_y, direction=start_direction)
         self.set_cell([snake_x, snake_y], CellType.snake)
@@ -43,12 +43,12 @@ class Grid:
 
     def generate_new_apple(self):
         """Generates a new apple in a random position that is not in the Snake or in another apple"""
-        x = random.randint(0, self.x_cells - 1)
-        y = random.randint(0, self.y_cells - 1)
+        x = random.randint(0, self.length - 1)
+        y = random.randint(0, self.height - 1)
         cell = [x, y]
         while cell in self.snake.body or self.get_cell(x, y) is CellType.apple:
-            x = random.randint(0, self.x_cells - 1)
-            y = random.randint(0, self.y_cells - 1)
+            x = random.randint(0, self.length - 1)
+            y = random.randint(0, self.height - 1)
             cell = [x, y]
         print("NEW APPLE")
         print(cell)
@@ -88,4 +88,4 @@ class Grid:
     def snake_died(self):
         """Checks to see if the snake is dead."""
         (x, y) = self.snake.head()
-        return (self.snake.head() in self.snake.tail()) or (x < 0 or self.x_cells <= x) or (y < 0 or self.y_cells <= y)
+        return (self.snake.head() in self.snake.tail()) or (x < 0 or self.length <= x) or (y < 0 or self.height <= y)
