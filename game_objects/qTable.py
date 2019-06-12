d611d0fa6e74b32c5d3e305f76d1218d4c10c790
medina <medinalamkin@yahoo.com>
2019-06-11 20:44:14 -0700

    using torch instead of numpy

--- game_objects/qTable.py
+++ game_objects/qTable.py
@@ -1,5 +1,5 @@
 import random
-import numpy as np
+import torch
 from game_objects.direction import Direction
 
 __X__ = 0
@@ -12,7 +12,7 @@ class qTable():
     discountRate = None
 
     def __init__(self, length, height, learnRate, discountRate):
-        self.table = np.zeros((length * height, 4))
+        self.table = torch.zeros((length, height, 4))
         self.learnRate = learnRate
         self.discountRate = discountRate
 
@@ -23,9 +23,9 @@ class qTable():
     def update(self, prevCell, curCell, direction, reward):
         px = prevCell[__X__]
         py = prevCell[__Y__]
-        """Update current cell with new Q value"""
-        self.table[px * py][direction] = self.table[px * py][direction] + self.learnRate * (reward + self.discountRate * self.nextAction(curCell) - self.table[px * py][direction])
-        # print(self.table[px * py][direction])
+        # Update current cell with new Q value
+        self.table[px][py][direction] = self.table[px][py][direction] + self.learnRate * (reward + self.discountRate * self.nextAction(curCell) - self.table[px][py][direction])
+        # print(self.table[px][py][direction])
 
     def nextAction(self, curCell):
         if curCell is None:
@@ -33,20 +33,21 @@ class qTable():
         else:
             cx = curCell[__X__]
             cy = curCell[__Y__]
-            return max(self.table[cx * cy])
+	    # DELETE COMMENT LATER: want to see all directions?
+            return max(self.table[cx][cy][:])
 
     """coordinate is a list of X and Y located at head of snake"""
     # Can possibly take the discountRate comparison out for testing purposes
     def getMax(self, coordinate):
         maxDir = None
-        curState = self.table[coordinate[__X__] * coordinate[__Y__]]
-        """if all state elements are equal or we choose to go in random direction"""
-        """random.random() < self.discountRate or """
-        if np.array_equal(curState, curState):
+        curState = self.table[coordinate[__X__]][coordinate[__Y__]][:]
+        # if all state elements are equal or we choose to go in random direction
+        # random.random() < self.discountRate or
+        if curState[0] == curState[1] and curState[0] == curState[2] and curState[0] == curState[3]:
             maxDir = random.randint(0, 3)
         else:
-            curState = np.tolist(curState)
-            """gets the max qtable value from all possible directions for deciding which direction to go"""
+            curState = curState.tolist()
+            # gets the max qtable value from all possible directions for deciding which direction to go
             maxDir = curState.index(max(curState))
-        """direction corosponding to the direction enum"""
+        # direction corresponding to the direction enum
         return Direction(maxDir)
